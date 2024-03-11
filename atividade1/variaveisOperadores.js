d1ebae9694ea3ef0cfb9e5ed5a598da9bac06003
GuilFranca <131270776+GuilFranca@users.noreply.github.com>
2024-03-11 10:10:07 -0300

    Add files via upload

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+var numero1 = 10;
+var numero2 = 5;
+
+var resultadoAdicao = numero1 + numero2;
+console.log("Resultado da adição:", resultadoAdicao);
+
+var resultadoSubtracao = numero1 - numero2;
+console.log("Resultado da subtração:", resultadoSubtracao);
+
+var resultadoMultiplicacao = numero1 * numero2;
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+
+var resultadoDivisao = numero1 / numero2;
+console.log("Resultado da divisão:", resultadoDivisao);
+
+var resultadoModulo = numero1 % numero2;
+console.log("Resultado do módulo:", resultadoModulo);
+
+var resultadoPotencia = Math.pow(numero1, numero2);
+console.log("Resultado da potência:", resultadoPotencia);
